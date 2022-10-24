59f92306f2467e6bfbd4db49ceaf3665c39fe108
Nader Hanna <nader.m.hanna@gmail.com>
2022-10-24 22:34:58 +0000

    I mean....it's kind of working but needs hella clean up

--- script.js
+++ script.js
@@ -20,15 +20,24 @@ const newRibbonWidth =
 const numberOfSegmentsToBuild = Math.ceil(ribbonElWidth / segmentElWidth) + 1;
 const animationDelayInterval = animationDuration / numberOfSegmentsToBuild;
 
+stylesheet.addStyle(`segment-base`, `.segment`, [
+  `left: ${-1 * segmentElWidth}px`,
+  `width: ${segmentElWidth}px`,
+]);
+
+segmentEl.remove();
 for (let i = 0; i < numberOfSegmentsToBuild; i++) {
+  console.log(">>>> building", i);
   ribbonEl.appendChild(segmentEl.cloneNode(true));
   stylesheet.addStyle(`segment-${i}`, `.segment:nth-child(${i + 1})`, [
     `animation-delay: ${i * animationDelayInterval}s`,
   ]);
 }
+stylesheet.addStyle("ribbon", ".ribbon", [`width: ${newRibbonWidth}px`]);
 stylesheet.addCssToDocument();
 
 dataEl.innerHTML = `current ribbon width: ${ribbonElWidth}px`;
 dataEl.innerHTML = `${dataEl.innerHTML} || segment width: ${segmentElWidth}px`;
 dataEl.innerHTML = `${dataEl.innerHTML} || num of segments: ${numberOfSegmentsToBuild}`;
 dataEl.innerHTML = `${dataEl.innerHTML} || new ribbon width: ${newRibbonWidth}`;
+dataEl.innerHTML = `${dataEl.innerHTML} || animation-delay: ${animationDelayInterval}`;
